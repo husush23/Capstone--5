3727b36043680ad1ac0972c4f73ca15c4c03ba45
husush23 <husseinkadare40@gmail.com>
2023-09-18 17:05:33 +0300

    Associations update and category form added

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+class CategoriesController < ApplicaionController
+    def index
+    end
+
+    def create
+    end
+
+    def new
+    end
+    
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
-end
+    belongs_to :user, class_name: 'User'
+    has_and_belongs_to_many :exchanges, join_table: :categories_exchanges
+    validates :name, presence: true
+  end

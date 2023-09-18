9bc3e6861ca75c397b3bdeb97b9108360eae8921
husush23 <husseinkadare40@gmail.com>
2023-09-18 15:32:08 +0300

    Models and associations completed

--- db/migrate/20230918121722_create_exchanges.rb
+++ db/migrate/20230918121722_create_exchanges.rb
@@ -1,12 +1,11 @@
-class Exchange < ActiveRecord::Migration[7.0]
+class CreateExchanges < ActiveRecord::Migration[7.0]
   def change
-    create_table :exchange do |t|
+    create_table :exchanges do |t|
       t.references :author, null: false, foreign_key: {to_table: :users}
       t.string :name
       t.decimal :amount
 
       t.timestamps
-
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_18_093408) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_18_122506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_exchanges", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "exchange_id", null: false
+    t.index ["category_id", "exchange_id"], name: "index_categories_exchanges_on_category_id_and_exchange_id"
+    t.index ["exchange_id", "category_id"], name: "index_categories_exchanges_on_exchange_id_and_category_id"
+  end
+
+  create_table "exchanges", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_exchanges_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_18_093408) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "exchanges", "users", column: "author_id"
 end

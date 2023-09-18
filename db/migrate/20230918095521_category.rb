39d570ad20eac11bd0193ac63daa6fb1b53d2a66
husush23 <husseinkadare40@gmail.com>
2023-09-18 13:11:15 +0300

    Exchange migrations added

--- db/migrate/20230918095521_category.rb
+++ db/migrate/20230918095521_category.rb
@@ -1,4 +1,11 @@
 class Category < ActiveRecord::Migration[7.0]
   def change
+    create_table :category do |t|
+      t.references :user, null: false, foreign_key: {to_table: :users} 
+      t.string :name
+      t.string :icon
+
+      t.timestamps
+    end
   end
 end
---
+++ db/migrate/20230918100233_exchange.rb
@@ -0,0 +1,12 @@
+class Exchange < ActiveRecord::Migration[7.0]
+  def change
+    create_table :exchange do |t|
+      t.references :author, null: false, foreign_key: {to_table: :users}
+      t.string :name
+      t.decimal :amount
+
+      t.timestamps
+
+    end
+  end
+end
